267016652679a9ee22ef3d49a4ffb89bce79a847
Rashi Tyagi <56127515+ratyagi@users.noreply.github.com>
2025-12-01 12:18:30 -0600

    Implement MessageBus class for message handling

--- core/message_bus.py
+++ core/message_bus.py
@@ -1,1 +1,17 @@
 # A2A message passing + routing
+class MessageBus:
+    def __init__(self):
+        self.agents = {}
+        self.queue = []
+
+    def register_agent(self, name: str, agent):
+        self.agents[name] = agent
+
+    def send(self, msg: AgentMessage):
+        self.queue.append(msg)
+
+    def run(self, session_id: str):
+        while self.queue:
+            msg = self.queue.pop(0)
+            if msg.receiver in self.agents:
+                self.agents[msg.receiver].handle_message(msg, self)
